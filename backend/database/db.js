2f929ee5091223d5bdf9a22a2d9c6b0653fcac2c
anurag <anuragmishra4499@gmail.com>
2024-09-20 17:32:38 +0530

    first commit

--- backend/database/db.js
+++ backend/database/db.js
@@ -4,7 +4,6 @@ require("dotenv").config()
 const DB_URL = process.env.DB_URL
 const connectToDatabase = async () => {
     try {
-        //    await mongoose.connect('mongodb+srv://anuragmishra9152:1OiTv9AjQaqJldyx@college-management.zpx6f.mongodb.net/?retryWrites=true&w=majority&appName=College-management')  
            await mongoose.connect(DB_URL)  
         console.log('Database connected successfully.');
     } catch (err) {
